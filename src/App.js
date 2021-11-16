be0661b86729162d08bef16904ee006b8f63926c
Talita Gregory Nunes Freire <talita.freire@dazn.com>
2021-11-16 17:53:26 +0100

    feat: random cats added

--- src/App.js
+++ src/App.js
@@ -1,10 +1,28 @@
 import ImageComponent from './components/ImageComponent'
 import cat100 from './images/gati-100.gif'
+import catAwe from './images/gati-awe.gif'
+import catGreat from './images/gati-great.gif'
+import catWin from './images/gati-win.gif'
+import catWow from './images/gati-wow.gif'
+import catYass from './images/gati-yass.gif'
+import getRandomItem from './core/functions'
 
 function App() {
+
+  const catImagesArray = [
+    cat100,
+    catAwe,
+    catGreat,
+    catWin,
+    catWow,
+    catYass,
+  ];
+
+  const randomCat = getRandomItem(catImagesArray)
+
   return (
     <div className="App">
-      <ImageComponent image={cat100}/>
+      <ImageComponent image={randomCat}/>
     </div>
   );
 }
